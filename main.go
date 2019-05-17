ad22b5f0476698b9b33d7d4a91bc9db7e8c9ebaa
eduardo <edu_paim@hotmail.com>
2019-05-17 01:59:21 -0300

    project

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
+
+func main() {
+	println("starting server...")
+	prometheus.MustRegister(prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "version",
+		Help: "Version information about this service",
+		ConstLabels: map[string]string{
+			"version": "v1.1.51",
+			"service": "demo",
+		},
+	}))
+	counter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "counterApp",
+		Help: "Counter test",
+	})
+	prometheus.MustRegister(counter)
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		println("receive request")
+		counter.Inc()
+		handler := promhttp.Handler()
+		handler.ServeHTTP(w, r)
+	})
+	if err := http.ListenAndServe(":2112", nil); err != nil {
+		panic(err)
+	}
+}
